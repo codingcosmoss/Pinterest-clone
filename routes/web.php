47dd926ac25204b25522b4b4675e60c7e1cd1fd3
Muhammadali <muhammadaliwebdeveloper@gmail.com>
2025-02-22 20:46:53 +0500

    bug

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return response()->json([
+        'status' => false,
+        'code' => 401,
+        'message' => "Avtorizatsiyadan o'tilmagan"
+    ]);
+});
